244db659e7f991ec51fbf9849f016e7400c22032
drincann <1019933576@qq.com>
2021-08-27 17:45:38 +0800

    feature: 添加了子包 asynclib.asynchttp, 实现了简单的 GET HTTP/1.0 基于事件驱动的异步请求接口

---
+++ asynclib/asynchttp.py
@@ -0,0 +1,54 @@
+import socket
+import urllib.parse
+from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
+from threading import Thread
+from .eventQueue import eventQueue
+
+
+def get(*, url, callback):
+    urlObj = urllib.parse.urlparse(url)
+    selector = DefaultSelector()
+    sock = socket.socket()
+    sock.setblocking(False)
+
+    def connected():
+        selector.unregister(sock.fileno())
+        selector.register(sock.fileno(), EVENT_READ, responded)
+        print(f"""GET {urlObj.path if urlObj.path != '' else '/'}{'?' if urlObj.query != '' else '' + urlObj.query} HTTP/1.0\r\n\r\n""")
+        sock.send(
+            f"""GET {urlObj.path if urlObj.path != '' else '/'}{'?' if urlObj.query != '' else '' + urlObj.query} HTTP/1.0\r\n\r\n"""
+            .encode('ascii')
+        )
+
+    responseData = bytes()
+
+    def responded():
+        nonlocal responseData
+        chunk = sock.recv(4096)
+        if chunk:
+            responseData += chunk
+        else:
+            selector.unregister(sock.fileno())
+            eventQueue.pushCallback(lambda: callback(responseData))
+            nonlocal __stop
+            __stop = True
+    __stop = False
+
+    def loop():
+        while True:
+            events = selector.select()
+            for event_key, event_mask in events:
+                cbk = event_key.data
+                cbk()
+            if __stop:
+                break
+
+    Thread(target=loop).start()
+    selector.register(sock.fileno(), EVENT_WRITE, connected)
+
+    try:
+        sock.connect(
+            (urlObj.hostname, urlObj.port if urlObj.port != None else 80)
+        )
+    except BlockingIOError:
+        pass
