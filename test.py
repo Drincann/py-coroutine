08eac23e1df845cdacd6ceaef751a94e026f2cd7
drincann <1019933576@qq.com>
2021-08-27 18:10:43 +0800

    feature: 将示例脚本的内容改为异步请求

--- test.py
+++ test.py
@@ -1,35 +1,18 @@
-import asyncio
-from asynclib import asyncRun, loop, Future
+from asynclib import asyncRun, loop, Future, asynchttp
 
 
-class container:
-    arr = list(range(10))
-
-    def __iter__(self):
-        self.i = -1
-        return self
-
-    def __next__(self):
-        self.i += 1
-        if self.i >= len(self.arr):
-            raise StopIteration
-        return self.arr[self.i]
+def http():
+    responseData = yield from Future(
+        lambda resolve:
+            asynchttp.get(
+                url='http://gaolihai.cool/doc/README.md',
+                callback=lambda response: resolve(response.decode('utf-8'))
+            )
+    )
+    # print(responseData)
 
 
-obj = container()
-
-
-def asyncIter(i):
-    for ele in obj:
-        yield from Future(lambda resolve: (print('协程挂起'), resolve(None)))
-        print(f'协程 {i} ------- {ele}')
-    return None
-
-
-asyncRun(asyncIter(1))
-print('同步')
-asyncRun(asyncIter(2))
-asyncRun(asyncIter(3))
-asyncRun(asyncIter(4))
+for i in range(10):
+    asyncRun(http())
 
 loop()
